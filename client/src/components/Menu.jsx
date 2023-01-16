c8a0af27a36cde9090b563d4ace1a3127d7661be
MarcoPavez <marcopz03@outlook.com>
2023-01-16 10:46:15 -0300

    cambios visuales en pagina publicaciones

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Menu = () => {
+
+    const posts = [
+        {
+          id: 1,
+          title: "AAA",
+          desc: "asdjasj",
+          img: "https://i.imgur.com/swzokWj_d.webp?maxwidth=520&shape=thumb&fidelity=high",
+        },
+        {
+          id: 2,
+          title: "BBB",
+          desc: "asdjasj",
+          img: "https://i.imgur.com/HxwP1bW_d.webp?maxwidth=520&shape=thumb&fidelity=high",
+        },
+        {
+          id: 3,
+          title: "CCC",
+          desc: "asdjasj",
+          img: "https://i.imgur.com/FQStsYR_d.webp?maxwidth=520&shape=thumb&fidelity=high",
+        },
+        {
+          id: 4,
+          title: "DDD",
+          desc: "asdjasj",
+          img: "https://i.imgur.com/l5p0pZi_d.webp?maxwidth=520&shape=thumb&fidelity=high",
+        },
+      ];
+  return (
+    <div className='menu'>
+        <h1>Otras publicaciones que te pueden interesar</h1>
+        {posts.map((post)=>(
+            <div className="post" key={post.id}>
+                <img src={post.img}/>
+                <h2>{post.title}</h2>
+                <button>Leer más</button>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default Menu
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu"
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://i.imgur.com/swzokWj_d.webp?maxwidth=520&shape=thumb&fidelity=high"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://i.imgur.com/swzokWj_d.webp?maxwidth=520&shape=thumb&fidelity=high"
+            alt=""
+          />
+          <div className="info">
+            <span>John</span>
+            <p>Publicado hace 2 días</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+            <img src="/" alt=""/>
+            </Link>
+            <img src="/" alt=""/>
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa quae, quo autem labore odio nemo consectetur aspernatur! Architecto deserunt enim corrupti, porro asperiores sit facere iusto iure non quod maiores!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum rem, itaque voluptas repellat quos quas ratione mollitia expedita maiores veritatis ab dicta perferendis vero libero quo voluptate sint eius odit.</p>
+      </div>
+      <Menu/>
+    </div>
+  );
+};
 
 export default Single;
