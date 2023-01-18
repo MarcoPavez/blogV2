8b8ca16aa285ffae01b13650d700209db6f1be1b
MarcoPavez <marcopz03@outlook.com>
2023-01-18 15:16:22 -0300

    ajusta ruta de autorizacion, crea funcion de registro en frontend

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,7 +5,7 @@ import { login, logout, register } from "../controllers/auth.js"
 const router = express.Router()
 
 /* Controlador para POST de informacion de registro a la bbdd */
-router.post("/register"),register
+router.post("/register",register)
 router.post("/login",login)
 router.post("/logout",logout)
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  //Luego de crear rutas y controladores, creamos las constantes que permitirán retener los datos ingresados por el usuario
+  //Para luego almacenarlos en la bbdd
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder='Nombre de usuario'></input>
-        <input required type="email" placeholder='Correo electrónico'></input>
-        <input required type="password" placeholder='Contraseña'></input>
+        <input
+          required
+          type="text"
+          placeholder="Nombre de usuario"
+          name="username"
+          onChange={handleChange}
+        ></input>
+        <input
+          required
+          type="email"
+          placeholder="Correo electrónico"
+          name="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          required
+          type="password"
+          placeholder="Contraseña"
+          name="password"
+          onChange={handleChange}
+        ></input>
         <button>Register</button>
         <p>El usuario y/o contraseña son incorrectos</p>
-        <span>¿Ya tienes una cuenta? <Link to="/login">Ingresa aquí</Link></span>
+        <span>
+          ¿Ya tienes una cuenta? <Link to="/login">Ingresa aquí</Link>
+        </span>
       </form>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Register
+export default Register;
