dd7d540d95c10fb0777e3c2942b903fc23f1befc
MarcoPavez <marcopz03@outlook.com>
2023-01-18 17:42:10 -0300

    crea login controller, instala jsonwebtoken

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import { db } from "../db.js"
 import bcrypt from "bcryptjs"
 
+
 export const register = (req,res) => {
     // CHECK si el usuario ya existe
     const q = "SELECT * FROM users WHERE email = ? or username = ?"
@@ -24,7 +25,7 @@ export const register = (req,res) => {
 
         //Insertamos los datos entregados por el usuario a la bbdd, el (?) son todos los datos que el
         //usuario nos entrega
-        const q ="INSER INTO users ('username','email','password') VALUES (?)"
+        const q ="INSERT INTO users(`username`,`email`,`password`) VALUES (?)"
         const values = [
             req.body.username,
             req.body.email,
@@ -40,6 +41,20 @@ export const register = (req,res) => {
 
 export const login = (req,res) => {
     
+    const q = "SELECT * FROM users WHERE username = ?"
+
+    db.query(q, [req.body.username], (err,data) => {
+        if (err) return res.json(err);
+        if (data.length === 0) return res.status(404).json("Usuario no existe")
+
+        //COMPROBAR CONTRASEÑA
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if(!isPasswordCorrect) return res.status(400).json("Usuario y/o contraseña incorrectos")
+
+
+    })
 }
 
 export const logout = (req,res) => {
