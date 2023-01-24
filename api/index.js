57ef510247f0bc9cd086f24ab516154f880fc0aa
MarcoPavez <marcopz03@outlook.com>
2023-01-24 19:14:05 -0300

    funcionalidad subir imagenes al cliente

--- api/index.js
+++ api/index.js
@@ -1,16 +1,33 @@
-import express, { application } from "express"
-import postRoutes from "./routes/posts.js"
-import userRoutes from "./routes/users.js"
-import authRoutes from "./routes/auth.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/posts", postRoutes)
-app.use("/api/users",userRoutes)
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8080, () => {
     console.log("Servidor conectado exitosamente en el puerto 8080")
